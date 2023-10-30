65bf4daaa6a8f126a0c3326f4946f1ee328083c5
Alexander Repeta <alexander.repeta@gmail.com>
2023-10-30 15:59:48 +0200

    prepare examples

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,3 @@
-import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Layout } from './Layout';
-
-const HomePage = lazy(() => import('pages/HomePage'));
-const QuizzesPage = lazy(() => import('pages/QuizzesPage'));
-const CreateQuizPage = lazy(() => import('pages/CreateQuizPage'));
-const QuizDetailsPage = lazy(() => import('pages/QuizDetailsPage'));
-const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
-
 export const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="create" element={<CreateQuizPage />} />
-        <Route path="quizzes" element={<QuizzesPage />} />
-        <Route path="quizzes/:quizId" element={<QuizDetailsPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
-  );
+  return <div>App</div>;
 };
